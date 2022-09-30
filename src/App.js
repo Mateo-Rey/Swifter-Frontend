e0e7577ff4112bd25c40caef7b9fc91b09dce17c
Mateo-Rey <jrey3darts@gmail.com>
2022-09-30 13:03:22 -0400

    firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
         
-      </header>
-    </div>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
