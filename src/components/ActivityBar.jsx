1158605d4827fffc3a78adcaddd4d05898dd8f83
Mateo-Rey <jrey3darts@gmail.com>
2022-10-05 11:19:53 -0400

    dashboard

--- src/components/ActivityBar.jsx
+++ src/components/ActivityBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { MdOutlineLibraryAdd } from "react-icons/md";
+import { RiHeartFill } from "react-icons/ri";
+import { TiHome } from "react-icons/ti";
+import {GrLineChart} from 'react-icons/gr'
 function ActivityBar() {
   return (
-    <div className='bg-emerald-500 h-10 md:w-[650px] w-96 z-10 fixed bottom-0'>
-      
+    <div className="bg-emerald-500 z-50 justify-between rounded px-5 z-100 md:px-2  py-1 flex h-10 md:w-[650px] w-96 fixed bottom-0">
+      <MdOutlineLibraryAdd size={35} />
+      <RiHeartFill size={35} />
+      <TiHome size={35} />
+      <GrLineChart size={35}/>
     </div>
-  )
+  );
 }
 
-export default ActivityBar
+export default ActivityBar;
--- src/components/SuggestedFollowers.jsx
+++ src/components/SuggestedFollowers.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 function SuggestedFollowers() {
   return (
     <div className='md:visible invisible fixed rounded-l top-0 right-0 bg-gradient-to-b from-emerald-600  to-blue-400  backdrop-blur-2xl h-full w-72'>
-        <div className='h-[99vh] rounded md:w-[280px] left-1 relative top-1 bg-white'>
-
+        <div className='h-[99vh] rounded md:w-[280px] left-1 relative top-1 bg-white/40'>
+          <h1 className='text-white font-bold text-2xl text-center'>
+            Suggested Followers
+          </h1>
         </div>
     </div>
   )
